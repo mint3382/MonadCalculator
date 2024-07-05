8069110b52f4c562880bc0c118cac18b9157712d
mint3382 <mskim0926@naver.com>
2024-07-06 02:04:33 +0900

    📕(docs) 프로젝트 파일 추가

---
+++ MonadCalculator/MonadCalculator/MonadCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonadCalculatorApp.swift
+//  MonadCalculator
+//
+//  Created by minsong kim on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MonadCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
