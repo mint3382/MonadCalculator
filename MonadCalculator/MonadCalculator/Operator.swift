c413d851911a657a75c6f033cd73e5519bd249bd
mint3382 <mskim0926@naver.com>
2024-11-18 23:48:21 +0900

    ⚙️(chore) Operator, ResultOperator 열거형 파일 분리

---
+++ MonadCalculator/MonadCalculator/Operator.swift
@@ -0,0 +1,15 @@
+//
+//  Operator.swift
+//  MonadCalculator
+//
+//  Created by minsong kim on 11/18/24.
+//
+
+import UIKit
+
+enum Operator: String {
+    case plus = "+"
+    case minus = "−"
+    case multiple = "×"
+    case divide = "÷"
+}
---
+++ MonadCalculator/MonadCalculator/ResultOperator.swift
@@ -0,0 +1,13 @@
+//
+//  ResultOperator.swift
+//  MonadCalculator
+//
+//  Created by minsong kim on 11/18/24.
+//
+
+import UIKit
+
+enum ResultOperator: String {
+    case enter = "="
+    case allClear = "AC"
+}
